8fdf2f831a879864ce0f8f57aeb4d91a961173bd
dawood76 <73113095+dawood76@users.noreply.github.com>
2020-11-26 00:01:19 +0500

    Update hypr.py

--- src/hypr.py
+++ src/hypr.py
@@ -10,7 +10,7 @@ print("wdddd", os.getcwd())
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--no-sandbox')
 #chrome_options.add_argument("--disable-setuid-sandbox")
 #driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
