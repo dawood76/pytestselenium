865331616f0d4727640dd5ff46c89e5888cf1a68
dawood76 <73113095+dawood76@users.noreply.github.com>
2020-11-25 23:59:47 +0500

    Update hypr.py

--- src/hypr.py
+++ src/hypr.py
@@ -10,10 +10,10 @@ print("wdddd", os.getcwd())
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--no-sandbox')
 #chrome_options.add_argument("--disable-setuid-sandbox")
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-
+#driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
 driver.get("http://localhost:4200/login")
 time.sleep(5)
 elem = driver.find_element_by_xpath('//input[@placeholder="Email"]')
