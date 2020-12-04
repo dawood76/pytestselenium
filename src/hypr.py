11d6100310d22ddf8459a27c5347888a7c4823c3
dawood76 <73113095+dawood76@users.noreply.github.com>
2020-12-05 00:58:21 +0500

    Update hypr.py

--- src/hypr.py
+++ src/hypr.py
@@ -4,32 +4,32 @@ from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 import time
 import os
+import pytest
+
+def test_login():
+  print("wdddd", os.getcwd())
+
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('window-size=1920x1080');
+  #chrome_options.add_argument("--disable-setuid-sandbox")
+  driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), chrome_options=chrome_options)
+  #driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+  driver.get("http://localhost:4200")
 
 
+  time.sleep(10)
 
-print("wdddd", os.getcwd())
+  elem = driver.find_element_by_xpath('//input[@placeholder="Email"]')
+  elem.send_keys("admin@hypr.pk")
+  elem = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+  elem.send_keys("12345678")
+  elem = driver.find_element_by_xpath('//button[text()="Log in"]')
+  elem.click()
+  time.sleep(3)
+  print(driver.get_screenshot_as_base64())
+  elem = driver.find_element_by_xpath('//a/span[text()="Products"]')
+  elem.click()
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('window-size=1920x1080');
-#chrome_options.add_argument("--disable-setuid-sandbox")
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), chrome_options=chrome_options)
-#driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
-driver.get("http://localhost:4200")
-
-
-time.sleep(10)
-
-elem = driver.find_element_by_xpath('//input[@placeholder="Email"]')
-elem.send_keys("admin@hypr.pk")
-elem = driver.find_element_by_xpath('//input[@placeholder="Password"]')
-elem.send_keys("12345678")
-elem = driver.find_element_by_xpath('//button[text()="Log in"]')
-elem.click()
-time.sleep(3)
-print(driver.get_screenshot_as_base64())
-elem = driver.find_element_by_xpath('//a/span[text()="Products"]')
-elem.click()
-
-print(driver.get_screenshot_as_base64())
+  print(driver.get_screenshot_as_base64())
